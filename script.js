a33c27c839b30556f12e01e3d564e4ca51996fa0
Vkscoma <vkscoma@gmail.com>
2023-04-24 10:07:19 -0700

    Adding more details

--- script.js
+++ script.js
@@ -1,8 +1,51 @@
-const addBookBtn = document.getElementById('add-book-btn');
-const modal = document.getElementById('modal');
+const addBookBtn = document.getElementById('add-book-btn'),
+    modal = document.getElementById('modal'),
+    subtmitBooks = document.getElementById('add-book-submit'),
+    modalForm = document.getElementById('modal--form'),
+    bookCard = document.querySelector('.card--container');
 
 addBookBtn.addEventListener('click', () => {
     modal.showModal();
 });
 
+subtmitBooks.addEventListener('click', (e) => {
+    e.preventDefault();
+    addBookToLibrary();
+    modalForm.reset();
+    modal.close();
+});
 
+let myLibrary = [];
+
+function Book(title, author, pages) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+}
+
+function addBookToLibrary() {
+    const title = document.getElementById('title').value,
+        author = document.getElementById('author').value,
+        pages = document.getElementById('pages').value;
+
+    // constructor function
+    const book = new Book(title, author, pages);
+    myLibrary.push(book);
+    // Create a new book element and add it to the DOM
+    const bookElement = document.createElement('div');
+    bookElement.classList.add('card--container');
+    bookElement.innerHTML = `
+    <h3>${book.title}</h3>
+    <p>${book.author}</p>
+    <p>${book.pages}</p>
+    <button class="card--container--btn delete">Remove</button>
+    `;
+    document.getElementById('books').appendChild(bookElement);
+    console.log(myLibrary);
+}
+
+bookCard.addEventListener('click', (e) => {
+    if (e.target && e.target.matches('button.delete')) {
+        console.log('clicked');
+    }
+});
