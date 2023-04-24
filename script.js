af0e8171d8bcbf62155c41e98ede1525ac2f5ec3
Vkscoma <vkscoma@gmail.com>
2023-04-24 08:06:52 -0700

    adding starter files

---
+++ script.js
@@ -0,0 +1,8 @@
+const addBookBtn = document.getElementById('add-book-btn');
+const modal = document.getElementById('modal');
+
+addBookBtn.addEventListener('click', () => {
+    modal.showModal();
+});
+
+
